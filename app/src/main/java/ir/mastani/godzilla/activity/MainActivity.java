d57b5ebfd5ea0c87be83be10a66d452691f7df99
Amin Mastani <amin.mastani@yahoo.com>
2017-09-15 18:33:15 +0430

    Init happy project

--- app/src/main/java/ir/mastani/godzilla/activity/MainActivity.java
+++ app/src/main/java/ir/mastani/godzilla/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package ir.mastani.godzilla;
+package ir.mastani.godzilla.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ir.mastani.godzilla.R;
 
 public class MainActivity extends AppCompatActivity {
 
